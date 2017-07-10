4d8a1534aa7cce40412d5681844e8077335ad019
lttxzmj <h.shoucheng@gmail.com>
2017-07-10 23:32:04 +0800

    add react-redux and rename file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import TodoList from './components/todo-list.jsx';
+import App from './components/app.jsx';
 
 render(
-  <TodoList /> ,
+  <App /> ,
   document.getElementById('root')
   )
